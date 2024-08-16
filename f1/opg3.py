21ab2628b1515537e7f3f9a2d7c03345c8c26452
Mikkel K Mosthaf <mosthaf@gmail.com>
2024-08-16 10:19:59 +0200

    Opdateret opgave 3

--- f1/opg3.py
+++ f1/opg3.py
@@ -16,9 +16,9 @@ for i in range(0, 20, 2):
   print(i)
 
 """" Opgaver
-- udskriv en liste af 
--
-- 
+- udskriv en liste af tal fra 1-50 - men kun de lige tal.
+- udskriv en liste af tal fra 50-1 - men kun de ulige tal.
+
 Brug cheatsheet eller google hvis I er i tvivl om syntaksen.
 """
 for kodesprog in ('python', 'c#', 'html'):
@@ -30,3 +30,16 @@ for kodesprog in ('python', 'c#', 'html'):
 kodesprog = ('python', 'c#', 'html')
 for i in range(3):
   print(kodesprog[i])
+
+for kodesprog in ('python', 'c#', 'html'):
+  # Vi vil kun have python
+  if kodesprog == "Python":
+    print(kodesprog)
+
+# hov - hvorfor virker sidste eksempel ikke som forventet? 
+
+"""" Opgaver
+- udskriv en liste af tal fra 1-50 - sæt en stjerne ved siden af tallet hvis det er et primtal. Tip: brug liste med primtal
+og if-blok og liste-funktionen "in"
+
+"""
