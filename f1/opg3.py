37b7e480cc4b5f05b59523b5b1df139ccdddc89e
Mikkel K Mosthaf <mosthaf@gmail.com>
2024-08-23 10:36:53 +0200

    uodated ex 3

--- f1/opg3.py
+++ f1/opg3.py
@@ -1,8 +1,22 @@
 """ demonstration af 
-loops med range() 
 loops med lister 
 """
+students = ['Peter', 'Anna', 'Pernille']
 
+"""" Opgave til lister
+Opret en liste med elever.
+- udskriv denne liste
+- tilføj 3 nye elever - find selv på navnene, sæt drengenavnene først
+- udskriv listen baglæns
+- udskriv alle navne i listen med store bogstaver
+- opret en ny liste der indeholder alle drengenavne og pigenavne med splice operatoren
+- udskriv alle navne fra indeks 2-5
+"""
+
+
+""" demonstration af 
+loops med range()
+"""
 for i in range(10):
   print(i)
 
@@ -15,29 +29,6 @@ for i in range(25, 10, -2):
 for i in range(0, 20, 2):
   print(i)
 
-"""" Opgaver
-- udskriv en liste af tal fra 1-50 - men kun de lige tal.
-- udskriv en liste af tal fra 50-1 - men kun de ulige tal.
-
-Brug cheatsheet eller google hvis I er i tvivl om syntaksen.
-"""
-for kodesprog in ('python', 'c#', 'html'):
-  print(kodesprog)
-
-for kodesprog in ('python', 'c#', 'html'):
-  print(kodesprog.upper())
-
-kodesprog = ('python', 'c#', 'html')
-for i in range(3):
-  print(kodesprog[i])
-
-for kodesprog in ('python', 'c#', 'html'):
-  # Vi vil kun have python
-  if kodesprog == "Python":
-    print(kodesprog)
-
-# hov - hvorfor virker sidste eksempel ikke som forventet? 
-
 # %%
 # alle tal til og med halvtreds som kan divideres med 5.
 for i in range(50):
@@ -45,13 +36,11 @@ for i in range(50):
     print(i, " kan divideres med 5")
 
 # Der er en fejl i ovenstående - kan I se den? 
+"""" Opgave til range()
+- udskriv en liste af tal fra 1-50 - men kun de lige tal.
+- udskriv en liste af tal fra 50-1 - men kun de ulige tal.
+- udskriv alle tal fra 1-50 men sæt en stjerne ved tallet hvis det er et primtal. Tip: definer en liste af primtal og test med den.
 
-"""" Opgaver
-- udskriv en liste af tal 1-50 - sæt en stjerne ved siden af tallet hvis det er et primtal. Tip: brug liste med primtal
-og if-blok og liste-funktionen "in"
-- udskriv en liste af tallene 1-50 med tallet selv og produktet (2.potens - slå det op)
-- lav en liste med alle navne i klassen - udskriv alle navne men uden vokaler! Tip - dobbelt loop.
-
+Brug cheatsheet eller google hvis I er i tvivl om syntaksen.
 """
 
-# %%
